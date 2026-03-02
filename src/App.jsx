88764f4487ced83ba27b8f7e12c5c3790740d726
徐梓崴 <xu768leo@gmail.com>
2026-03-02 20:42:49 +0800

    統一register路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-// 引入我們剛剛做好的兩個頁面
+// 引入頁面
 import LoginPage from './pages/Login/LoginPage';
 import MainPage from './pages/MainPage/MainPage';
 import RegisterPage from './pages/Register/RegisterPage';
-import RegisterSuccessPage from './pages/Register/RegisterSuccessPage';
 
 const App = () => {
   return (
@@ -21,7 +20,6 @@ const App = () => {
         
         {/* 註冊頁面 */}
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/register-success" element={<RegisterSuccessPage />} />
 
         {/* 如果亂打網址，通通導回登入頁 */}
         <Route path="*" element={<Navigate to="/" replace />} />
--- src/pages/Register/RegisterPage.jsx
+++ src/pages/Register/RegisterPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../../App.css';
 import AppHeader from '../../components/Header/AppHeader';
@@ -8,11 +8,16 @@ import PrimaryButton from '../../components/Button/PrimaryButton';
 
 const RegisterPage = () => {
   const navigate = useNavigate();
+  const [isSuccess, setIsSuccess] = useState(false);
 
   const handleRegister = () => {
     // 這裡未來可以加入後端註冊 API
-    // 目前直接跳轉到「註冊成功頁」
-    navigate('/register-success');
+    // 目前顯示註冊成功頁
+    setIsSuccess(true);
+  };
+
+  const handleGoHome = () => {
+    navigate('/home');
   };
 
   return (
@@ -24,35 +29,52 @@ const RegisterPage = () => {
 
         <AppHeader />
 
+        {isSuccess ? (
+          /* 成功訊息 */
+          <div style={{ margin: '40px 0', textAlign: 'center' }}>
+            <h2 style={{ fontSize: '30px', fontWeight: 'bold' }}>帳號建立成功！</h2>
+          </div>
+        ) : null}
+
         <div className="login-form">
-            {/* 欄位標籤 + 輸入框 1 */}
-            <div className="input-group-register">
-            <label>電子郵件地址</label>
-            <input type="email" />
-            </div>
-
-            {/* 欄位標籤 + 輸入框 2 */}
-            <div className="input-group-register">
-            <label>使用者名稱</label>
-            <input type="text" />
-            </div>
-
-            {/* 欄位標籤 + 輸入框 3 */}
-            <div className="input-group-register">
-            <label>密碼</label>
-            <input type="password" />
-            </div>
-
-            {/* 欄位標籤 + 輸入框 4 */}
-            <div className="input-group-register">
-            <label>確認密碼</label>
-            <input type="password" />
-            </div>
-
-            {/* 建立帳戶按鈕 */}
-            <PrimaryButton onClick={handleRegister}>
-            建立帳戶
+          {isSuccess ? (
+            /* 登入按鈕 */
+            <PrimaryButton onClick={handleGoHome}>
+              登入
             </PrimaryButton>
+          ) : (
+            /* 註冊表單 */
+            <>
+              {/* 欄位標籤 + 輸入框 1 */}
+              <div className="input-group-register">
+              <label>電子郵件地址</label>
+              <input type="email" />
+              </div>
+
+              {/* 欄位標籤 + 輸入框 2 */}
+              <div className="input-group-register">
+              <label>使用者名稱</label>
+              <input type="text" />
+              </div>
+
+              {/* 欄位標籤 + 輸入框 3 */}
+              <div className="input-group-register">
+              <label>密碼</label>
+              <input type="password" />
+              </div>
+
+              {/* 欄位標籤 + 輸入框 4 */}
+              <div className="input-group-register">
+              <label>確認密碼</label>
+              <input type="password" />
+              </div>
+
+              {/* 建立帳戶按鈕 */}
+              <PrimaryButton onClick={handleRegister}>
+              建立帳戶
+              </PrimaryButton>
+            </>
+          )}
         </div>
 
         <Navigation position="bottom" />
